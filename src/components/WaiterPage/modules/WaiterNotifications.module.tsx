5dce453a7bab29cc4ea0596c65484a7c9c7ad806
0xZonaV <zonavofficial@gmail.com>
2023-03-03 00:34:50 +1000

    Hardcoded notification module

--- src/components/WaiterPage/modules/WaiterNotifications.module.tsx
+++ src/components/WaiterPage/modules/WaiterNotifications.module.tsx
@@ -1,6 +1,16 @@
 import {WaiterPageRenderType} from "../../../../@types";
 import {FC} from "react";
 import WaiterPageFooter from "@/components/WaiterPage/components/Footer.component";
+import {
+    WaiterTableListBox,
+    WaiterTableListContentBox,
+    WaiterTableListTitle
+} from "@/components/WaiterPage/styles/WaiterTableList.style";
+import {
+    WaiterNotification,
+    WaiterNotificationsBox,
+    WaiterNotificationText, WaiterNotificationTime
+} from "@/components/WaiterPage/styles/WaiterNotifications.style";
 
 type ProfileProps = {
     waiterPageRender: WaiterPageRenderType,
@@ -10,6 +20,70 @@ type ProfileProps = {
 const WaiterNotificationsModule: FC<ProfileProps> = ({ setWaiterPageRender, waiterPageRender }) => {
     return(
         <>
+            <WaiterTableListBox>
+                <WaiterTableListContentBox>
+                    <WaiterTableListTitle>
+                        Мои уведомления
+                    </WaiterTableListTitle>
+
+                    <WaiterNotificationsBox>
+                        <WaiterNotification>
+                            <WaiterNotificationText>
+                                Стол A1:  добавлен новый заказ
+                            </WaiterNotificationText>
+                            <WaiterNotificationTime>
+                                11:03
+                            </WaiterNotificationTime>
+                        </WaiterNotification>
+
+                        <WaiterNotification>
+                            <WaiterNotificationText>
+                                Стол A2: выбрана оплата наличными
+                            </WaiterNotificationText>
+                            <WaiterNotificationTime>
+                                11:01
+                            </WaiterNotificationTime>
+                        </WaiterNotification>
+
+                        <WaiterNotification>
+                            <WaiterNotificationText>
+                                Стол B3: счет успешно оплачен
+                            </WaiterNotificationText>
+                            <WaiterNotificationTime>
+                                10:56
+                            </WaiterNotificationTime>
+                        </WaiterNotification>
+
+                        <WaiterNotification>
+                            <WaiterNotificationText>
+                                Стол B1: добавлено примечание к заказу
+                            </WaiterNotificationText>
+                            <WaiterNotificationTime>
+                                10:49
+                            </WaiterNotificationTime>
+                        </WaiterNotification>
+
+                        <WaiterNotification>
+                            <WaiterNotificationText>
+                                Стол A2: вас просят подойти
+                            </WaiterNotificationText>
+                            <WaiterNotificationTime>
+                                10:35
+                            </WaiterNotificationTime>
+                        </WaiterNotification>
+
+                        <WaiterNotification>
+                            <WaiterNotificationText>
+                                Стол B2: добавлен новый заказ
+                            </WaiterNotificationText>
+                            <WaiterNotificationTime>
+                                10:30
+                            </WaiterNotificationTime>
+                        </WaiterNotification>
+                    </WaiterNotificationsBox>
+
+                </WaiterTableListContentBox>
+            </WaiterTableListBox>
             <WaiterPageFooter waiterPageRender={waiterPageRender} setWaiterPageRender={setWaiterPageRender} />
         </>
     )
---
+++ src/components/WaiterPage/styles/WaiterNotifications.style.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const WaiterNotificationsBox = styled.div`
+  display: block;
+  margin-top: 20px;
+`
+
+export const WaiterNotification = styled.div`
+  background: #FFFFFF;
+  border-radius: 12px;
+  width: 326px;
+  min-height: 68px;
+
+  display: inline-block;
+  margin-top: 30px;
+  padding: 23px 11px 11px 23px ;
+`
+
+export const WaiterNotificationText = styled.div`
+  font-family: 'Doloman Pavljenko';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 19px;
+  display: flex;
+  align-items: center;
+
+  color: #000000;
+
+  
+`
+
+export const WaiterNotificationTime = styled.div`
+  font-family: 'Doloman Pavljenko';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 13px;
+  line-height: 13px;
+
+  color: #000000;
+  text-align: right;
+`
